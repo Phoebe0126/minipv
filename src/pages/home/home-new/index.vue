1a3ad4436fde9ac4cc953827380121cf52a1cf93
Phoebe0126 <1542756558@qq.com>
2020-04-28 19:28:25 +0800

    修改一些报错

--- src/pages/home/home-new/index.vue
+++ src/pages/home/home-new/index.vue
@@ -1,6 +1,6 @@
 <template>
     <view>
-        分类
+        
     </view>
 </template>
 
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,6 +1,5 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
 			<text class="title">{{title}}</text>
 		</view>
@@ -28,13 +27,6 @@
 		text-align: center;
 		height: 400upx;
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
 	.title {
 		font-size: 36upx;
 		color: #8f8f94;
